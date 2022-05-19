ba0dfbf6b208f177eaf8c28798b90cb711ba206e
SmokedKoala <k.azarov@inbox.ru>
2022-05-19 21:54:07 +0300

    bonfire functionality

---
+++ Assets/scripts/CreateFire.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFire : MonoBehaviour
+{
+    public string tagName;
+    private int num;
+    public GameObject Prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (num == 2){
+            createFire();
+        }
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == tagName){
+            num++;
+            Destroy(coll.gameObject);
+        }
+    }
+
+    private void createFire(){
+        Instantiate(Prefab, transform.position, transform.rotation);
+        Destroy(this.gameObject);
+    }
+}
