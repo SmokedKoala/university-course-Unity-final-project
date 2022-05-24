7ee922773edd622c79b382085be74f60df2e4cc7
Nikita <huskyray@mail.ru>
2022-05-25 00:23:36 +0300

    upd

--- Assets/scripts/GrabAndDrop.cs
+++ Assets/scripts/GrabAndDrop.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HELP : MonoBehaviour
+public class GrabAndDrop : MonoBehaviour
 {
     public Transform player;
     public Transform playerCam;
