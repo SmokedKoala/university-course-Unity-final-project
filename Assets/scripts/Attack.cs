7b2d977a8b1a6d8f630c1745695c7ee66a59eb1f
Nikita <huskyray@mail.ru>
2022-05-30 22:22:58 +0300

    add: player attack on enemy

--- Assets/scripts/Attack.cs
+++ Assets/scripts/Attack.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-
     Animator anim;
     private int current_tool = 0;
+
+    CharacterCombat combat;
 
     private SwitchTools tools;
 
@@ -14,6 +15,7 @@ public class Attack : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         tools = GameObject.Find("tools").GetComponent<SwitchTools>();
+        combat = PlayerManager.instance.player.GetComponent<CharacterCombat>();
     }
 
     void Update()
@@ -22,7 +24,24 @@ public class Attack : MonoBehaviour
         {
             current_tool = tools.selectedTool;
             if (current_tool == 1)
+            {
                 anim.SetTrigger("knife_attack_trigger");
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 15))
+                {
+                    Transform target = hit.collider.transform;
+                    float distance = Vector3.Distance(target.position, transform.position);
+                    if (distance <= 3f)
+                    {
+                        CharacterStats targetStats = target.GetComponent<CharacterStats>();
+                        if (targetStats != null)
+                        {
+                            combat.Attack(targetStats);
+                        }
+                    }
+                }
+            }
         }
     }
 }
